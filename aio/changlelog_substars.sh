f03106e787ef0b006309c941ae235a9ece3f0de7
vazmin <chwingwong@outlook.com>
2022-04-13 00:41:37 +0800

    fix act

--- aio/changlelog_substars.sh
+++ aio/changlelog_substars.sh
@@ -16,8 +16,11 @@ REVISION=$2
 
 MODULE_TMP_SUBSTVARS=substvars-$(echo $RANDOM | md5sum | head -c 12)
 
-awk -F'=' 'FNR==NR {a[$1] = $2; next} $1 in a {print $1"="$2}' $MODULE_BASE_DIR/debian/substvars substvars > /tmp/$MODULE_TMP_SUBSTVARS
-cp /tmp/$MODULE_TMP_SUBSTVARS $MODULE_BASE_DIR/debian/substvars
+ORIGIN_SUBSTVARS=$MODULE_BASE_DIR/debian/substvars
+if [ -f ${ORIGIN_SUBSTVARS} ]; then
+    awk -F'=' 'FNR==NR {a[$1] = $2; next} $1 in a {print $1"="$2}'  substvars > /tmp/$MODULE_TMP_SUBSTVARS
+    cp /tmp/$MODULE_TMP_SUBSTVARS $ORIGIN_SUBSTVARS
+fi
 cd $MODULE_BASE_DIR
 dch --distribution unstable -v $REVISION -M "upgrade to $REVISION"
 git add debian
